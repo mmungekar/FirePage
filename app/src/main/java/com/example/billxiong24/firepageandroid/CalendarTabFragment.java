352b1f0c623e78f90ff407bf75ca54bb6eb22a0d
Mina Mungekar <mina.mungekar@duke.edu>
2017-03-13 00:51:35 -0400

    need to get files to show up

--- app/src/main/java/com/example/billxiong24/firepageandroid/CalendarTabFragment.java
+++ app/src/main/java/com/example/billxiong24/firepageandroid/CalendarTabFragment.java
@@ -1,10 +1,19 @@
 package com.example.billxiong24.firepageandroid;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
 
 /**
  * Created by Ritler on 3/8/17.
@@ -12,12 +21,60 @@ import android.view.ViewGroup;
 
 public class CalendarTabFragment extends Fragment {
 
+    private int activeButton = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         View rootView = inflater.inflate(R.layout.tab_calendar, container, false);
 
+        HashSet<Date> events = new HashSet<>();
+        events.add(new Date());
+
+        CalendarView cv = ((CalendarView)findViewById(R.id.calendar_view));
+        cv.updateCalendar(events);
+        Button orange = (Button) findViewById(R.id.button7);
+        whenClicked(orange);
+      /*  orange.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                whenClicked(v);
+            }
+        });*/
+        Button blue = (Button) findViewById(R.id.button8);
+        whenClicked(blue);
+
+        // assign event handler
+        cv.getGrid().setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> view, View cell, int position, long id)
+            {
+                if(activeButton!=0) {
+                    Button temp = (Button) findViewById(activeButton);
+                    cell.setBackgroundColor(((ColorDrawable) temp.getBackground()).getColor());
+                }
+            }
+        });
+        cv.setEventHandler(new CalendarView.EventHandler()
+        {
+            @Override
+            public void onDayLongPress(Date date)
+            {
+                // show returned day
+                DateFormat df = SimpleDateFormat.getDateInstance();
+                Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return rootView;
+    }
+    private void whenClicked(Button b){
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                activeButton = v.getId();
+            }
+        });
     }
 
 }
