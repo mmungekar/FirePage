dbad48fa4f9697d59d2789a08748339bdb0cd226
Mina Mungekar <mina.mungekar@duke.edu>
2017-03-15 00:41:17 -0400

    account settings and resolve page progress

--- app/src/main/java/com/example/billxiong24/firepageandroid/SettingsTabFragment.java
+++ app/src/main/java/com/example/billxiong24/firepageandroid/SettingsTabFragment.java
@@ -16,6 +16,16 @@ public class SettingsTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_settings, container, false);
+    Button nameButton =  (Button) findViewById(R.id.nameButton);
+        nameButton.setOnClickListener(new View.OnClickListener() {
+                                                  @Override
+                                                  public void onClick(View view) {
+
+                                                  }
+                                              });
+        Button numberButton =  (Button) findViewById(R.id.numberButton);
+        Button logoutButton =  (Button) findViewById(R.id.logoutButton);
+        Button passwordButton =  (Button) findViewById(R.id.passwordButton);
 
         return rootView;
     }
