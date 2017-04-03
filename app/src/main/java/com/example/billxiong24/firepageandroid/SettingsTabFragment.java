e7423d4f93bfcd713557f248bd11d023ea441e96
Mina Mungekar <mina.mungekar@duke.edu>
2017-04-03 01:43:35 -0400

    Settings page popups implemented

--- app/src/main/java/com/example/billxiong24/firepageandroid/SettingsTabFragment.java
+++ app/src/main/java/com/example/billxiong24/firepageandroid/SettingsTabFragment.java
@@ -1,63 +1,63 @@
-//package com.example.billxiong24.firepageandroid;
-//
-//import android.support.v4.app.Fragment;
-//import android.os.Bundle;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.Button;
-//import android.widget.EditText;
-//
-///**
-// * Created by Ritler on 3/8/17.
-// */
-//
-//public class SettingsTabFragment extends Fragment {
-//    View rootView;
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        rootView = inflater.inflate(R.layout.tab_settings, container, false);
-//    Button nameButton =  (Button)rootView.findViewById(R.id.nameButton);
-//        nameButton.setOnClickListener(new View.OnClickListener() {
-//                                                  @Override
-//                                                  public void onClick(View view) {
-//                                                      rootView = inflater.inflate(R.layout.tab_settings_name, container, false);
-//                                                      addFields();
-//                                                  }
-//                                              });
-//        Button numberButton =  (Button) rootView.findViewById(R.id.numberButton);
-//        numberButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                rootView = inflater.inflate(R.layout.tab_settings_number, container, false);
-//                addFields();
-//            }
-//        });
-//        Button passwordButton =  (Button) rootView.findViewById(R.id.passwordButton);
-//        passwordButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                rootView = inflater.inflate(R.layout.tab_password, container, false);
-//                addFields();
-//            }
-//        });
-//        Button logoutButton =  (Button) rootView.findViewById(R.id.logoutButton);
-//
-//        return rootView;
-//    }
-//
-//    private void addFields(){
-//        EditText t1 = (EditText)rootView.findViewById(R.id.editText4);
-//        EditText t2 = (EditText)rootView.findViewById(R.id.editText5);
-//        Button enterButton =(Button)rootView.findViewById(R.id.button4);
-//        enterButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //if password and other fields are valid
-//                rootView.setContentView(R.layout.tab_settings);
-//            }
-//        });
-//    }
-//
-//}
+package com.example.billxiong24.firepageandroid;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+
+/**
+ * Created by Ritler on 3/8/17.
+ */
+
+public class SettingsTabFragment extends Fragment {
+    View rootView;
+    PopupWindow myWindow;
+    View popupView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.tab_settings, container, false);
+    Button nameButton =  (Button)rootView.findViewById(R.id.nameButton);
+        addListener(nameButton,inflater.inflate(R.layout.tab_settings_name,null));
+       Button numberButton =  (Button) rootView.findViewById(R.id.numberButton);
+        addListener(numberButton,inflater.inflate(R.layout.tab_settings_number,null));
+        Button passwordButton =  (Button) rootView.findViewById(R.id.passwordButton);
+       addListener(passwordButton,inflater.inflate(R.layout.tab_settings_password,null));
+        Button logoutButton =  (Button) rootView.findViewById(R.id.logoutButton);
+
+        return rootView;
+    }
+
+    private void addFields(View myView){
+       EditText t1 = (EditText)myView.findViewById(R.id.editText4);
+        EditText t2 = (EditText)myView.findViewById(R.id.editText5);
+        Button enterButton =(Button)myView.findViewById(R.id.button4);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //if password and other fields are valid
+               myWindow.dismiss();
+            }
+        });
+    }
+
+    private void addListener(Button b, View v){
+        final View myView = v;
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myWindow = new PopupWindow(myView, ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+                myWindow.setFocusable(true);
+                myWindow.showAtLocation(myView, Gravity.CENTER, 0, 0);
+                addFields(myView);
+            }
+        });
+    }
+
+}
